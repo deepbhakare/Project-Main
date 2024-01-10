92fb46bc43f6255436e124814805db497e72f07f
deepbhakare <136368452+deepbhakare@users.noreply.github.com>
2024-01-10 14:46:48 +0530

    Update MAIN.py

--- MAIN.py
+++ MAIN.py
@@ -8,10 +8,7 @@ def main():
     col1, col2, col3 = st.beta_columns(3)  # Using beta_columns to create two columns
 
     if col1.button("Vegetable Plant"):
-        st.success("Some well-known websites that provide useful information on growing vegetables include- 1.The Old Farmer's Almanac
-2.[University Extension Websites](e.g., University of California Cooperative Extension, Cornell University - Gardening Resources)
-3.Gardening Know How
-4.Royal Horticultural Society (RHS)")
+        st.success("NOTIFACTION-(https://www.almanac.com/gardening/growing-guides#Vegetable)")
         # Redirect to the Vegetable Plant website (replace the URL with the actual website)
         st.markdown("[Vegetable Plant Website](https://vegetable.streamlit.app)")
 
