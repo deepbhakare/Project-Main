e4105d8a37b0bc5d0913d5036d7e8553e684468d
deepbhakare <136368452+deepbhakare@users.noreply.github.com>
2024-01-10 15:09:51 +0530

    Update MAIN.py

--- MAIN.py
+++ MAIN.py
@@ -10,17 +10,17 @@ def main():
     if col1.button("Vegetable Plant"):
         st.success("Website that provide useful information on growing vegetables https://www.almanac.com/gardening/growing-guides#Vegetable")
         # Redirect to the Vegetable Plant website (replace the URL with the actual website)
-        st.markdown("Click Here --> [Vegetable Plant Website](https://vegetable.streamlit.app)")
+        st.write("Click Here --> [Vegetable Plant Website](https://vegetable.streamlit.app)")
 
     if col2.button("Cash Crop"):
         st.success("Website that provide useful information on growing Cotton Plant https://cottontoday.cottoninc.com/cotton-and-water-better-management-of-an-increasingly-scarce-resource)"
         # Redirect to the Cash Crop website (replace the URL with the actual website)
-        st.markdown("Click Here --> [Cash Crop Website](https://cash-crop.streamlit.app)")
+        st.write("Click Here --> [Cash Crop Website](https://cash-crop.streamlit.app)")
 
     if col3.button("Cash Crop (BETA)"):
         st.success("Note - ")
         # Redirect to the Cash Crop website (replace the URL with the actual website)
-        st.markdown("Click Here --> [Cash Crop [BETA] Website](https://cpddpy-qwhhpsykdudfcplonnvrr7.streamlit.app)")
+        st.write("Click Here --> [Cash Crop [BETA] Website](https://cpddpy-qwhhpsykdudfcplonnvrr7.streamlit.app)")
 
 if __name__ == "__main__":
     main()
