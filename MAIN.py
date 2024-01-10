02cee4220f9d113d8782f92ed747cb314a7ab6df
deepbhakare <136368452+deepbhakare@users.noreply.github.com>
2024-01-10 14:37:26 +0530

    Update MAIN.py

--- MAIN.py
+++ MAIN.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 def main():
     st.title("Crop Disease Classification and Treatment Recommendation System for Amravati District")
-    st.title("Objective: The goal of this project is to develop a robust crop disease classification system using machine learning techniques. The primary objective is to accurately identify and classify diseases affecting various crops, enabling early detection and timely intervention for crop protection.")
+    st.write("Objective: The goal of this project is to develop a robust crop disease classification system using machine learning techniques. The primary objective is to accurately identify and classify diseases affecting various crops, enabling early detection and timely intervention for crop protection.")
 
     # Create two buttons side by side
     col1, col2, col3 = st.beta_columns(3)  # Using beta_columns to create two columns
